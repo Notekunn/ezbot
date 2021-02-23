efc4b5146d5fe29e67fdf0a1594b3d5503119d29
Cuong Tran <clonebmn2itt@gmail.com>
2021-02-23 17:39:22 +0700

    update

--- src/plugins/CustomBot/index.ts
+++ src/plugins/CustomBot/index.ts
@@ -4,20 +4,24 @@ interface Options {
   name: string;
   admins: string[];
 }
+const details = {
+  name: 'Custom Bot',
+  version: '1.0.1',
+  description: 'Custom your bot',
+};
 export class CustomBot extends CustomPlugin {
   constructor(options: Options) {
-    const details = {
-      name: 'Custom Bot',
-      version: '1.0.1',
-      description: 'Custom your bot',
-    };
     super(details, (bot) => {
       bot.setOptions(options);
       bot.on('start', () => {
         bot.emitInfo(`Prefix: ${bot.getOptions().prefix}`);
         bot.emitInfo(`Name: ${bot.getOptions().name}`);
       });
-      const helloPattern = [/bot ơi/i, new RegExp(options.name, 'gi'), new RegExp(`Accio`, 'i')];
+      const helloPattern = [
+        /bot ơi/i,
+        new RegExp(options.name || 'EZ-Bot', 'gi'),
+        new RegExp(`Accio`, 'i'),
+      ];
       bot.hear(helloPattern, (payload, chat) => {
         const { name, prefix } = bot.getOptions();
         chat.say(`${name} ready!\n` + `Prefix: ${prefix}`, () => {
